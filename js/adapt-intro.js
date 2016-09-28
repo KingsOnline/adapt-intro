ea5d8a217e346755b83d840cfe6c2fd0e8da9d17
Simon  Date <simondate@yahoo.co.uk>
2016-09-28 11:45:37 +0100

    reverted to non-min code and changed to postRender

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -1,12 +1,15 @@
 define(function(require) {
 
     var Adapt = require('coreJS/adapt');
-    var Backbone = require('backbone');
-    var introJs = require('extensions/adapt-introjs/js/intro.min.js');
+    var pageView = require('core/js/views/pageView');
+    var introJs = require('./intro');
 
     // This should add/update progress on menuView
-    Adapt.on('adapt:initialize', function(view) {
-    	introJs().start();
+
+	Adapt.on("pageView:postRender", function() {
+    	if($("#wrapper").hasClass("location-page")){
+    		introJs().start();
+    	}
     });
 
 });
