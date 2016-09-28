5c578219cfacfe6bd59a53e2a1232212c272c2e6
Simon  Date <simondate@yahoo.co.uk>
2016-09-28 15:47:30 +0100

    Created function to tutorial elements to the page using Javascript.

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -6,10 +6,25 @@ define(function(require) {
 
     // This should add/update progress on menuView
 
-	Adapt.on("pageView:postRender", function() {
+	Adapt.on("pageView:ready", function() {
+		console.log('hello world');
     	if($("#wrapper").hasClass("location-page")){
+	    	assignTutorial("page-level-progress-navigation","This bar shows your progress on the page. You can click it to see what components you have left to complete.");
+			assignTutorial("page-header","The content of the course goes here. Scroll down to read it!");
+
     		introJs().start();
     	}
-    });
+    }); function myFunction() {
 
-});
+}
+
+
+
+});	
+
+function assignTutorial(className, text){
+    var h1 = document.getElementsByClassName(className)[0];
+    var att = document.createAttribute("data-intro");
+    att.value = text;
+    h1.setAttributeNode(att);
+}
