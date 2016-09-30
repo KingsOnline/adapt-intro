5e15df887d28935befb1d99e125a20dd07d9fe1c
Simon  Date <simondate@yahoo.co.uk>
2016-09-30 10:08:23 +0100

    changed to sit outside extensions object

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -7,7 +7,8 @@ define(function(require) {
     // This should add/update progress on menuView
 
 	Adapt.on("pageView:ready", function() {
-		var introJs = Adapt.course.get('_globals')._extensions._introJs;
+		var introJs = Adapt.course.get('_globals')._introJs;
+        console.log(introJs);
         if(introJs === undefined) return;
     	if($("#wrapper").hasClass("location-page") && introJs._isEnabled && introJs._steps[0] != null){
            for (i = 0; i < introJs._steps.length; i++) { 
