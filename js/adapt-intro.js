588c4d6aba936a35e680fb706929efc06bd50070
Simon  Date <simondate@yahoo.co.uk>
2016-09-30 11:41:29 +0100

    Moved to outside _globals folder

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -7,9 +7,9 @@ define(function(require) {
     // This should add/update progress on menuView
 
 	Adapt.on("pageView:ready", function() {
-		var introJs = Adapt.course.get('_globals')._extensions._introJs;
+		var introJs = Adapt.course.attributes._introJs;
         console.log(Adapt.course);
-        console.log(Adapt.course.get('_globals'));
+        console.log(introJs);
         if(introJs === undefined) return;
     	if($("#wrapper").hasClass("location-page") && introJs._isEnabled && introJs._steps[0] != null){
            for (i = 0; i < introJs._steps.length; i++) { 
