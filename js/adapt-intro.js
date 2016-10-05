15e295401eba0ae2e1ec3e4facd0cd1c28b28047
Simon  Date <simondate@yahoo.co.uk>
2016-10-05 11:39:46 +0100

    Renamed to 'adapt-intro' to make it seem less "developer-y"

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -2,20 +2,18 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var pageView = require('core/js/views/pageView');
-    var introJsLibary = require('./intro');
+    var introJs = require('./intro');
 
     // This should add/update progress on menuView
 
 	Adapt.on("pageView:ready", function() {
-		var introJs = Adapt.course.attributes._introJs;
-        console.log(Adapt.course);
-        console.log(introJs);
-        if(introJs === undefined) return;
-    	if($("#wrapper").hasClass("location-page") && introJs._isEnabled && introJs._steps[0] != null){
-           for (i = 0; i < introJs._steps.length; i++) { 
-                assignTutorial(introJs._steps[i]._element, introJs._steps[i].text);
+		var intro = Adapt.course.attributes._intro;
+        if(intro === undefined) return;
+    	if($("#wrapper").hasClass("location-page") && intro._isEnabled && intro._steps[0] != null){
+           for (i = 0; i < intro._steps.length; i++) { 
+                assignTutorial(intro._steps[i]._element, intro._steps[i].text);
             }
-    		introJsLibary().start();
+    		introJs().start();
     	}
     });
 });	
