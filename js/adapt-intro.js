317f2ea6b0a4b3a0aea0ca7cc28a9c01a3a721f6
Simon  Date <simondate@yahoo.co.uk>
2016-10-07 15:59:20 +0100

    Added adapt-intro.less, fixed handlebars. Improved code and created a function to check that prevents introJS from launching multiple times.

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -3,36 +3,35 @@ define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
     var IntroView = Backbone.View.extend({
 
         initialize: function() {
+            var data = this.model.toJSON();
+            var template = Handlebars.templates.intro;
+            data._globals = Adapt.course.get("_globals");
+            this.setElement(template(data)).$el.prependTo($(".navigation-inner"));
             this.listenTo(Adapt, {
                 "navigation:openIntro": this.startIntro
             }).render();
         },
 
         render: function() {
-            var data = this.model.toJSON();
-            var template = Handlebars.templates.intro;
-
-            console.log(data);
-            console.log(template);
-
             var intro = Adapt.course.attributes._intro;
             if(intro === undefined) return;
             if($("#wrapper").hasClass("location-page") && intro._isEnabled && intro._steps[0] != null){
-               for (i = 0; i < intro._steps.length; i++) { 
+               for (i = 0; i < intro._steps.length; i++) {
                     assignTutorial(intro._steps[i]._element, intro._steps[i].text);
                 }
-                introJs().start();
             }
-
-            data._globals = Adapt.course.get("_globals");
-            this.setElement(template(data)).$el.prependTo($(".navigation-inner"));
         },
 
-        startIntro: function() {
-            console.log('g');
-            introJs().start();
+        startIntro: function(){
+          $('.navigation-intro').attr('disabled',true);
+            introJs().start().oncomplete(function() {
+              console.log('complete');
+              $('.navigation-intro').attr('disabled',false);
+            }).onexit(function() {
+              console.log('exit');
+              $('.navigation-intro').attr('disabled',false);
+          });
         }
-
     });
 
     function assignTutorial(className, text){
@@ -43,25 +42,9 @@ define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
         h1.setAttributeNode(att);
     }
 
-    function onBeforeUnload(config) {
-        return !Adapt.course.get("_isComplete") ?
-            config.browserPromptIfIncomplete || undefined :
-            config.browserPromptIfComplete || undefined;
-    }
-
     Adapt.once("adapt:initialize", function() {
         var config = Adapt.course.get("_intro");
-
         if (!config) return;
-
-        var button = config._button;
-
-
-        new IntroView({ model: new Backbone.Model(button) });
-
-        if (config.browserPromptIfIncomplete || config.browserPromptIfComplete) {
-            $(window).on("beforeunload", _.partial(onBeforeUnload, config));
-        }
+        new IntroView({ model: new Backbone.Model() });
     });
-
 });
