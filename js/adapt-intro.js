3477693402a7ff27559889c87af039bb215888e8
Simon  Date <simondate@yahoo.co.uk>
2016-10-07 13:28:14 +0100

    Used code from adapt-close to convert into a backbone element that uses a model.

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -1,19 +1,38 @@
-define(function(require) {
+define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
 
-    var Adapt = require('coreJS/adapt');
-    var introJs = require('./intro');
+    var IntroView = Backbone.View.extend({
 
-    // This should add/update progress on menuView
+        initialize: function() {
+            this.listenTo(Adapt, {
+                "navigation:openIntro": this.startIntro
+            }).render();
+        },
 
-	Adapt.on("pageView:ready", function() {
-		var intro = Adapt.course.attributes._intro;
-        if(intro === undefined) return;
-    	if($("#wrapper").hasClass("location-page") && intro._isEnabled && intro._steps[0] != null){
-           for (i = 0; i < intro._steps.length; i++) { 
-                assignTutorial(intro._steps[i]._element, intro._steps[i].text);
+        render: function() {
+            var data = this.model.toJSON();
+            var template = Handlebars.templates.intro;
+
+            console.log(data);
+            console.log(template);
+
+            var intro = Adapt.course.attributes._intro;
+            if(intro === undefined) return;
+            if($("#wrapper").hasClass("location-page") && intro._isEnabled && intro._steps[0] != null){
+               for (i = 0; i < intro._steps.length; i++) { 
+                    assignTutorial(intro._steps[i]._element, intro._steps[i].text);
+                }
+                introJs().start();
             }
-    		introJs().start();
-    	}
+
+            data._globals = Adapt.course.get("_globals");
+            this.setElement(template(data)).$el.prependTo($(".navigation-inner"));
+        },
+
+        startIntro: function() {
+            console.log('g');
+            introJs().start();
+        }
+
     });
 
     function assignTutorial(className, text){
@@ -23,4 +42,26 @@ define(function(require) {
         att.value = text;
         h1.setAttributeNode(att);
     }
+
+    function onBeforeUnload(config) {
+        return !Adapt.course.get("_isComplete") ?
+            config.browserPromptIfIncomplete || undefined :
+            config.browserPromptIfComplete || undefined;
+    }
+
+    Adapt.once("adapt:initialize", function() {
+        var config = Adapt.course.get("_intro");
+
+        if (!config) return;
+
+        var button = config._button;
+
+
+        new IntroView({ model: new Backbone.Model(button) });
+
+        if (config.browserPromptIfIncomplete || config.browserPromptIfComplete) {
+            $(window).on("beforeunload", _.partial(onBeforeUnload, config));
+        }
+    });
+
 });
