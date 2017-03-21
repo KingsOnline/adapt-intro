cf1fb41564f27dc4b3708052f465e4487fa548a7
simondate <simondate@yahoo.co.uk>
2017-03-21 10:32:28 +0000

    rewrote to support appearing on the menu as well as the pages.

--- js/adapt-intro.js
+++ js/adapt-intro.js
@@ -1,4 +1,4 @@
-define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
+define(["coreJS/adapt", "./intro"], function(Adapt, introJs) {
 
     var IntroView = Backbone.View.extend({
 
@@ -12,25 +12,25 @@ define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
         },
 
         getElements: function(path) {
-            if($("#wrapper").hasClass("location-page") && path._isEnabled && path._steps[0] != null){
-               for (i = 0; i < path._steps.length; i++) {
+            if (path._isEnabled && path._steps[0] != null) {
+                for (i = 0; i < path._steps.length; i++) {
                     this.assignIntro(path._steps[i]._element, path._steps[i].text);
                 }
             }
         },
 
-        startIntro: function(){
-          var introPath = Adapt.course.attributes._intro;
+        startIntro: function() {
+            var introPath = Adapt.course.attributes._intro;
             this.getElements(introPath);
-            $('.navigation-intro').attr('disabled',true); // prevents the button being pressed again during introduction.
+            $('.navigation-intro').attr('disabled', true); // prevents the button being pressed again during introduction.
             introJs().setOptions(introPath._options).start().oncomplete(function() {
-              $('.navigation-intro').attr('disabled',false);
+                $('.navigation-intro').attr('disabled', false);
             }).onexit(function() {
-              $('.navigation-intro').attr('disabled',false);
+                $('.navigation-intro').attr('disabled', false);
             });
         },
 
-        assignIntro: function(className, text){
+        assignIntro: function(className, text) {
             var h1 = document.getElementsByClassName(className)[0];
             if (h1 === undefined) return;
             var att = document.createAttribute("data-intro");
@@ -40,26 +40,29 @@ define([ "coreJS/adapt", "./intro" ], function(Adapt, introJs) {
     });
 
     Adapt.on("pageView:preRender", function() {
-        if(this.course.attributes._intro._showOn != undefined) { // if not defined assume all pages
-          if(this.course.attributes._intro._showOn.length > 0){
-            if(jQuery.inArray(this.location._currentId, this.course.attributes._intro._showOn) === -1){
-              $('.navigation-intro').hide();
-              return; // skip the next if statement that creates adapt-intro
-            }
-          }
+        if (this.course.attributes._intro._showOn != undefined && this.course.attributes._intro._showOn.length > 0 && jQuery.inArray(this.location._currentId, this.course.attributes._intro._showOn) === -1) { // if not defined assume all pages
+            $('.navigation-intro').hide();
+        } else {
+            showOrCreate();
         }
+    });
 
-        if($('.navigation-intro').length){ // If you have been on a page before show.
-          $('.navigation-intro').show();
+    Adapt.on('menuView:preRender', function() { // hide on menu
+        if (this.course.attributes._intro._showOn != undefined && this.course.attributes._intro._showOn.length > 0 && !(this.course.attributes._intro._showOnMenu == true)) { // if not defined assume all pages
+            $('.navigation-intro').hide();
+        } else {
+            showOrCreate();
+        }
+    });
+
+    function showOrCreate() {
+        if ($('.navigation-intro').length) { // If you have been on a page before show.
+            $('.navigation-intro').show();
         } else { // create the element when you load into your first page.
-          var jsonExists = this.course.get("_intro");
-          if (!jsonExists) return;
-          new IntroView({ model: new Backbone.Model() });
+            if (Adapt.course.get("_intro") == undefined) return;
+            new IntroView({
+                model: new Backbone.Model()
+            });
         }
-    });
-
-    Adapt.on('menuView:preRender', function(view) { // hide on menu
-        $('.navigation-intro').hide();
-    });
-
+    }
 });
